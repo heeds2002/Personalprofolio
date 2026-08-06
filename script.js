c79545ec8b813ba8fc5bd0b7194db986e95cc3a6
heeds2002 <burtoneheeds2002@gmail.com>
2026-08-06 15:13:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+AOS.init({
+  duration: 700,   // keep animations quick and subtle
+  once: true,      // animate elements in only once, not every scroll pass
+  offset: 60,
+  easing: "ease-out-quart",
+});
